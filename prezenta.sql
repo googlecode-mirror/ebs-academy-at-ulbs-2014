7d9d1f54daa204710c479bc4b393c67405443615
icontiu <icontiu@gmail.com>
2014-12-05 11:33:51 +0000

    bun

--- prezenta.sql
+++ prezenta.sql
@@ -1,12 +1,14 @@
 CREATE TABLE IF NOT EXISTS `prezenta_user` (
-`id` int(11) NOT NULL,
-  `id_prezenta` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+`id` int(11)  NULL,
+  `id_prezenta` int(11)  NULL,
+  `id_user` int(11) NULL
+  PRIMARY KEY (`id`)
 )
 
 CREATE TABLE IF NOT EXISTS `prezenta` (
-`id` int(11) NOT NULL,
-  `id_materie` int(11) NOT NULL,
-  `data` date NOT NULL,
-  `tip_prezenta` varchar(15) NOT NULL
+`id` int(11)  NULL,
+  `id_materie` int(11)  NULL,
+  `data` date  NULL,
+  `tip_prezenta` varchar(15)  NULL
+  PRIMARY KEY (`id`)
 )
