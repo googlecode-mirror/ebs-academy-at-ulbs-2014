bca6be3f6b4b236ad39176b63f3577d4b160ec03
robert.albu <robert.albu@ulbsibiu.ro>
2014-12-11 21:15:50 +0000

    v2

--- teme_licenta.sql
+++ teme_licenta.sql
@@ -1,15 +1,15 @@
 CREATE TABLE ebs.teme_licenta (
-	id INT NOT NULL AUTO_INCREMENT,
-	id_materie INT(11) NOT NULL,
-	id_user INT(11) NOT NULL,
-	denumire_tema VARCHAR(50),
-	descriere_tema VARCHAR(500),
-	id_grupa INT,
-	locuri_disponibile TINYINT(2) NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY(id_materie) REFERENCES ebs.materii(id),
-	FOREIGN KEY(id_user) REFERENCES ebs.user(id),
-	FOREIGN KEY(id_grupa) REFERENCES ebs.grupa(id),
+ id INT NOT NULL AUTO_INCREMENT,
+  id_materie INT(11) NOT NULL,
+  id_user INT(11) NOT NULL,
+  denumire_tema VARCHAR(50),
+  descriere_tema VARCHAR(500),
+  id_grupa INT,
+  locuri_disponibile TINYINT(2) NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_materie) REFERENCES ebs.materii(id),
+  FOREIGN KEY(id_user) REFERENCES ebs.user(id),
+  FOREIGN KEY(id_grupa) REFERENCES ebs.grupa(id),
 );
 
 	
