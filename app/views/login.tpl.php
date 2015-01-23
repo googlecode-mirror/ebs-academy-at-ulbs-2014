60efa223b665642d1ab111f324504df5659765e7
icontiu <icontiu@gmail.com>
2015-01-23 12:20:24 +0000

    Formular

--- app/views/login.tpl.php
+++ app/views/login.tpl.php
@@ -1,9 +1,36 @@
-<html>
-	<head>
-		<title> Login </title>
-	</head>
-	<body>
-		<h1> Va rugam introduceti datele de logare: <h1>
-		<TABLE>blablabla</TABLE><head>confliiiiict!!!! :P</head>>
-	</body>
-</html>	
+<HTML>
+<HEAD>
+    <TITLE>Login</TITLE>
+</HEAD>
+
+<body>
+<h1> Va rugam introduceti datele de logare: </h1>
+    <table>
+                      <form method="POST" action="verificare.php">
+                            <tr>
+                                <td>Email</td>
+                                <td> <input type="email" name="email" /></td>
+                            </tr>
+
+                            <tr>
+                                <td>Password</td>
+                                <td><input type="password" name="password" /></td>
+                            </tr>
+                            
+                            
+                            <tr>
+                                <td><input type="submit" value="Submit" /></td>
+                                <td><input type="reset" value="Reset" /></td>
+                            </tr>
+							<tr>
+                               <td colspan="2" ><a href="#">Recover password</a></td>
+				
+                                
+                            </tr>
+                      </form>     
+
+                      
+    </table>
+
+</body>
+</HTML>
