f85664a978d6f76b41333166266de3e428a930ee
icontiu@gmail.com <icontiu@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-01-30 11:28:17 +0000

    meniu

---
+++ trunk/app/views/meniu.php
@@ -0,0 +1,9 @@
+
+		
+			<ul class="meniu_list">
+					<li><a href="#">Tema Licenta</a></li>
+					<li><a href="#">Noutati</a></li>
+					<li><a href="#">Prezenta</a></li>
+					<li><a href="#">Suport Curs</a></li>
+					<li><a href="#">Teste</a></li>
+			</ul>
