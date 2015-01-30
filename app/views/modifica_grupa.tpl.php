9254fe80ab96ecc95558e0bf232890ad605fe6e7
robert.albu <robert.albu@ulbsibiu.ro>
2015-01-30 10:12:11 +0000

    modifica grupa

---
+++ app/views/modifica_grupa.tpl.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="UTF-8">
+    <title>Modifica grupa</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<form>
+	<form method="post" action="#">
+		<table>
+			<tr>
+				<td>Nume</td>
+				<td><input type="text" name="nume" style="width:150px"/></td>
+			</tr>
+			<tr>
+				<td>An</td>
+				<td><input type="text" name="an" style="width:150px"/></td>
+			</tr>
+			<tr>
+				<td>Sef profil</td>
+				<td><input type="text" name="sef_profil" style="width:150px"/></td>
+			</tr>
+			<tr>
+				<td>Profil</td>
+				<td><input type="text" name="profil" style="width:150px"/></td>
+			</tr>
+			<tr>
+				<td colspan="2" style="text-align:right"><input type="button" value="Submit"/></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
