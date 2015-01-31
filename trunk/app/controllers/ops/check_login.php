8ddc70789ddc7d406d1aac4a6233fc23de6cf50c
adrian.brezaie@gmail.com <adrian.brezaie@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-01-31 11:16:33 +0000

    model and controller for User

---
+++ trunk/app/controllers/ops/check_login.php
@@ -0,0 +1,8 @@
+<?php
+function _check_login() {
+	//TODO
+	//$user = new User(getdbh());
+	//$user->checkPassword();
+	//redirect fie catre main page fie inapoi catre login
+	//$_POST contine datele din form trimise de user
+}
---
+++ trunk/app/models/User.php
@@ -0,0 +1,21 @@
+<?php
+class User {
+
+	function __construct($db) {
+		$this->db = $db;
+	}
+
+	public function checkPasswd($email, $pass) {
+
+	}
+
+
+	public function getUserDetails($user_id) {
+
+	}
+
+
+
+
+
+}
