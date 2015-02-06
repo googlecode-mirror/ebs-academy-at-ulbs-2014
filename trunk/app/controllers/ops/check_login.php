8923a931d684fe8c7d5e5b69603554a1f332e149
alintichindelean@gmail.com <alintichindelean@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-02-06 10:32:52 +0000

    create check_login.php

--- trunk/app/controllers/ops/check_login.php
+++ trunk/app/controllers/ops/check_login.php
@@ -5,4 +5,13 @@ function _check_login() {
 	//$user->checkPassword();
 	//redirect fie catre main page fie inapoi catre login
 	//$_POST contine datele din form trimise de user
+require_login();    
+  $user=new User();
+  if($user->checkPassword($_POST['úser'],$_POST['pass']) ){
+        redirect('main/index');
+    }
+      else{
+        redirect('main/login');
+      
+  }
 }
