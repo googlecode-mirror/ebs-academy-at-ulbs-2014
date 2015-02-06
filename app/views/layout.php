76414b0825ce8b8658d5754a62751ad3eb82e38f
bejan.catalin.alexandru <bejan.catalin.alexandru@gmail.com>
2015-02-06 10:56:31 +0000

    aranjare layout.php

--- app/views/layout.php
+++ app/views/layout.php
@@ -1,11 +1,27 @@
-<html>
-<head>
-	<title><?php echo $GLOBALS['sitename'];?></title>
-	<link rel="stylesheet" type="text/css" <?php echo 'href="'.WEB_DOMAIN.'/css/style.css"';?> />
-</head>
-<body>
-	<?php include "header.tpl.php";?>
-	<?php echo (isset($msg) && is_array($msg)) ? implode("\n",$msg) : ''?>
-	<?php include "footer.tpl.php";?>
-</body>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title><?php echo $GLOBALS['sitename'];?></title>
+		<link rel="stylesheet" type="text/css" <?php echo 'href="'.WEB_DOMAIN.'/css/style.css"';?> />
+	</head>
+
+	<body>
+		<div id="frame">
+			<div id="header">
+				<?php include "header.tpl.php"?>
+			</div>
+			
+			<div id="meniu_div">
+				<?php include "meniu.php"?>
+			</div>
+
+			<div id="main">
+				<?php include "login.tpl.php"?>
+			</div>
+			
+			<div id="footer">
+				<?php include "footer.tpl.php"?>
+			</div>
+		</div>
+    </body>
 </html>
