ee220bae64f38c4e181980f1fb86d1385d4b0b52
icontiu <icontiu@gmail.com>
2015-02-16 18:08:06 +0000

    refs #5 logout.php->> done

--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -1,3 +1,7 @@
 <div id="header">
-	<p> Welcome to ULBSPlatform User <a href="#">Deconectare</a></p>
+    <?php
+	if(isset($_SESSION['uid'])) echo '<p> Welcome to ULBSPlatform User <a href="'.myUrl('main/logout').'">Deconectare</a></p>';
+        else 
+           echo '<p> Welcome to ULBSPlatform User <a href="'.myUrl('main/logout').'">Login</a></p>';
+                ?>
 </div>
