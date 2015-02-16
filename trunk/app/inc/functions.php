f94491a2e1edfecc511b42785eb231e7ef13fdb3
icontiu@gmail.com <icontiu@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-02-16 17:46:05 +0000

    refs #2 Functia a fost creata

--- trunk/app/inc/functions.php
+++ trunk/app/inc/functions.php
@@ -23,4 +23,30 @@ function getUserType() {
 	return $_SESSION['user_type'];
 }
 
+function sendEmail($subject, $body, $from, $to) {
+	require_once APP_PATH . 'helpers/PHPMailer-master/class.phpmailer.php';
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTPDebug = 1;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'tls://smtp.gmail.com';
+        $mail->Port = '465';
+        $mail->isMail(true);
+        $mail->Username = 'icontiu@gmail.com';
+        $mail->Password = 'pass';
+        $mail->setFrom($from);
+        $mail->Subject =$subject;
+        $mail->Body = $body;
+        $mail->addAddress($to);
+
+        if ($mail->send()) {
+
+          return true;
+        } else {
+
+           return false;
+        }
+}
+
 
