33045bbf7eaf2d7f3afd655e10b9fe326eba2c68
icontiu@gmail.com <icontiu@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-02-23 12:44:15 +0000

    refs #9 logare cu datele primite de pe formular + criptare parola la logare

--- trunk/app/controllers/ops/check_login.php
+++ trunk/app/controllers/ops/check_login.php
@@ -7,10 +7,13 @@ function _check_login() {
 	//$_POST contine datele din form trimise de user
      
   $user=new User(getdbh());
-  $user_details = $user->checkPassword('test', 'test');
+  $user_details = $user->checkPassword($_POST['email'], $_POST['password']);
+  
   if(count($user_details) == 1){
 		//TODO adauga in sesiune user id, si user type ($user_details['ID'])
 		$_SESSION['uid'] = $user_details[0]['ID'];
+                $_SESSION['type'] = $user_details[0]['TIP'];
+               
         redirect('main/index');
     }
       else{
