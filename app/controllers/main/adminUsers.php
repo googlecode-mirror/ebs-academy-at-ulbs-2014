25f67107bad5f9e506651e2fd1492b9404ab748c
bejan.catalin.alexandru <bejan.catalin.alexandru@gmail.com>
2015-02-25 20:42:37 +0000

    inbunatatire a controllerului de administrare useri

--- app/controllers/main/adminUsers.php
+++ app/controllers/main/adminUsers.php
@@ -1,26 +1,69 @@
 <?php
-function _adminUsers() {
-//$_POST['checkbox']  = "id_1"- inseamna ca in interfata s-a selectat checkbox pt user_id 1
 
-	switch ($_POST['actiune']) {
-		case 'edit':
-			$user_id = explode("_", $_POST['1'])[1];
-			$user = new User(getdbh());
-			$user_details = $user->getUserDetails($user_id);
-			$result['user'] = $user_details;
-			$data['msg'][]=View::do_fetch(VIEW_PATH.'modifica_user.tpl.php', $result);
-			View::do_dump(VIEW_PATH.'layout.php',$data);
-			//$user->updateUser($id, $nume, $prenume, $status);
-			break;
-		case 'delete':
-			$user_id = explode("_", $_POST['1'])[1];
-			$user = new User(getDbh());
-			$user->deleteUser($user_id);
-			$data['msg'][]=View::do_fetch(VIEW_PATH.'afisare_user.tpl.php');
-			View::do_dump(VIEW_PATH.'layout.php',$data);
-			break;
-		default:
-		//echo "wrong action"
-		break;
-	}
+function _adminUsers() {
+
+    switch ($_POST['actiune']) {
+        
+        case 'edit':
+            reset($_POST);
+            $key = key($_POST);
+            $user_id = explode("_", $key);
+            
+            $user = new User(getDbh());
+            $user_details = $user->getUserDetails($user_id[1]);
+            
+            $result['user'] = $user_details;
+            $data['msg'][]=View::do_fetch(VIEW_PATH.'modifica_user.tpl.php', $result);
+            View::do_dump(VIEW_PATH.'layout.php',$data);
+            break;
+		
+        case 'delete':
+            reset($_POST);
+            $key = key($_POST);
+            $user_id = explode("_", $key);
+
+	    $user = new User(getDbh());
+            if ($user->deleteUser($user_id[1]) == true) {
+                $data['msg'][] = " Userul a  fost sters cu success";
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            } 
+            else {
+                $data['msg'][] = " Userul nu a fost sters";
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            }
+            break;
+            
+        case 'delete_all':           
+            $sterse = 0;
+            $nesterse = 0;
+            $user = new User(getDbh());
+            foreach ($_POST as $key) {
+                if ($key == 'delete_all') {
+                    continue;
+                } 
+                else {
+                    $user_id = explode("_", $key);
+                    if ($user->deleteUser($user_id[1]) == true) {
+                        $sterse++;
+                    } 
+                    else {
+                        $nesterse++;
+                    }
+                }
+            }
+
+            if ($sterse > 0) {
+                $data['msg'][] = $sterse . "useri au fost stersi cu success";
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            } 
+            else {
+                $data['msg'][] = $nesterse . "useri nu au fost stersi";
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            }
+            break;
+            
+        default:
+            //echo "wrong action"
+            break;
+    }
 }
