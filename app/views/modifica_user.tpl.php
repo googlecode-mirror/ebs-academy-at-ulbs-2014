9380b3fea46fda97d025d6d9bd8764e91a194612
bejan.catalin.alexandru <bejan.catalin.alexandru@gmail.com>
2015-02-25 15:18:44 +0000

    Tichet 2: pe view-ul de modifica_user, trebuie rezolvate urmatoarele:
    - adaugat action la form, catre controller de updateUser
    - adaugat value la fiecare input element, cu datele pre-incarcate ale userului (ex: email field)
    - fix reset button
    - adaugare user type

--- app/views/modifica_user.tpl.php
+++ app/views/modifica_user.tpl.php
@@ -1,28 +1,44 @@
-
-<form method="post" action="#">
+<form method='POST' action="<?php echo myUrl('ops/updateUser'); ?>" >
     <table>
         <tr>
             <td>Email</td>
-            <td> <input type="text" name="username" 
-			value="<?php echo isset($user[0]['EMAIL']) ? $user[0]['EMAIL'] : '';?>" /></td>
+            <td> <input type="email" name="email" style="width:150px"
+			            value="<?php echo isset($user[0]['EMAIL']) ? $user[0]['EMAIL'] : '';?>" /></td>
         </tr>
+
         <tr>
             <td>Nume</td>
-            <td><input type="text" name="nume" /></td>
+            <td><input type="text" name="nume" style="width:150px"
+                       value="<?php echo isset($user[0]['NUME']) ? $user[0]['NUME'] : '';?>" /></td>
         </tr>
+
         <tr>
             <td>Prenume</td>
-            <td><input type="text" name="prenume" /></td>
+            <td><input type="text" name="prenume" style="width:150px"
+                       value="<?php echo isset($user[0]['PRENUME']) ? $user[0]['PRENUME'] : '';?>" /></td>
         </tr>
+
         <tr>
             <td>Tip</td>
-            <td><input type="text" name="type" /></td>
+            <td><input type="text" name="type" style="width:150px"
+                       value="<?php echo isset($user[0]['TIP']) ? $user[0]['TIP'] : '';?>" /></td>
         </tr>
+
         <tr>
             <td>Data</td>
-            <td><input type="date" name="data" /></td>
+            <td><input type="date" name="data" style="width:150px"
+                       value="<?php echo isset($user[0]['DATAADAUGARII']) ? $user[0]['DATAADAUGARII'] : '';?>" /></td>
         </tr>
+
         <tr>
+            <td>Status</td>
+            <td><input type="text" name="data" style="width:150px"
+                       value="<?php echo isset($user[0]['STATUS']) ? $user[0]['STATUS'] : '';?>" /></td>
+        </tr>
+
+        <tr>
+            <td><input type="hidden" name="idUser"
+                       value="<?php echo isset($user[0]['ID']) ? $user[0]['ID'] : ''; ?>" >
             <td><input type="submit" value="Submit" /></td>
             <td><input type="reset" value="Reset" /></td>
         </tr>
