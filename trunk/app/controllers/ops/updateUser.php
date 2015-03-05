ab30bb58cee6c10600b932ff3bb6f0b16c19b81a
bejan.catalin.alexandru@gmail.com <bejan.catalin.alexandru@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-03-05 11:16:58 +0000

    Dupa modificarea sau stergerea unui user ramanem la lista de useri. (tichet 13)

--- trunk/app/controllers/ops/updateUser.php
+++ trunk/app/controllers/ops/updateUser.php
@@ -10,7 +10,7 @@ function _updateUser() {
     $result = $user->updateUser($_POST['idUser'], $_POST['email'], $_POST['nume'], $_POST['prenume'], $_POST['type'], $_POST['status']);
     if ($result) {
         $data['msg'][] = 'Userul a fost modificat cu success';
-        //$data['redirect'][]='administrare/show_users';
+        $data['redirect'][]='administrare/show_users';
         View::do_dump(VIEW_PATH . 'layout.php', $data);
     } else {
         $data['msg'][] = "Eroare la modificarea userului";
