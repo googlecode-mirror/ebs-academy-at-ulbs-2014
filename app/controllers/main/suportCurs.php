5d82f0fb9e1696510cc9c268258d5b3f301366cc
robert.tudora.mihai <robert.tudora.mihai@gmail.com>
2015-03-06 12:43:39 +0000

    Controller pentru Suport curs catre 2 view-uri (student,profesor)

---
+++ app/controllers/main/suportCurs.php
@@ -0,0 +1,30 @@
+<?php
+function _suportCurs() {
+isUserLoggedIn();
+
+if(getUserType()=='student'){
+	$data['msg'][]=View::do_fetch(VIEW_PATH.'suport_curs_stud.php');
+	View::do_dump(VIEW_PATH.'layout.php',$data);
+    }
+else{
+        $grupa=new Grupa(getdbh());
+        $getGroups=$grupa->fetchAll();
+        $result['grupa']=$getGroups;
+        
+        $materie=new Materii(getdbh());
+        $getMaterii=$materie->fetchAll();
+        $result['materie']=$getMaterii;
+                
+        $data['msg'][]=View::do_fetch(VIEW_PATH.'suport_curs_prof.php',$result);
+        View::do_dump(VIEW_PATH.'layout.php',$data);
+    }
+
+}
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
