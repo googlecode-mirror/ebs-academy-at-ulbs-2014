618689bba2f3e520ff02751becbf8c4aa70d7a27
bejan.catalin.alexandru <bejan.catalin.alexandru@gmail.com>
2015-03-07 17:57:14 +0000

    Adaugarea optiuni de stergere a mai multor useri dintr-o grupa sau chiar stergerea tuturor userilor din grupa

--- app/controllers/administrare/deleteGrupaMember.php
+++ app/controllers/administrare/deleteGrupaMember.php
@@ -2,18 +2,57 @@
 
 function _deleteGrupaMember() {
     isUserLoggedIn();
-    $key = key($_POST);
-    $grupaId = explode("_", $key);
 
-    $grupa = new Grupa(getDbh());
-    $deleteMember = $grupa->deleteGrupaMember($grupaId[1]);
-    if ($deleteMember) {
-        $data['msg'][] = "Studentul a fost scos din grupa";
-        $data['redirect'][] = 'administrare/show_grup';
-        View::do_dump(VIEW_PATH . 'layout.php', $data);
-    } else {
-        $data['msg'][] = "Eroare la stergerea din grupa";
-        $data['redirect'][] = 'administrare/show_grup';
-        View::do_dump(VIEW_PATH . 'layout.php', $data);
+    switch ($_POST['actiune']) {
+        
+        case 'delete':
+            
+            reset($_POST);
+            $key = key($_POST);
+            $grupaId = explode("_", $key);
+
+            $grupa = new Grupa(getDbh());
+            $deleteMember = $grupa->deleteGrupaMember($grupaId[1]);
+            if ($deleteMember) {
+                $data['msg'][] = "Studentul a fost scos din grupa";
+                $data['redirect'][] = 'administrare/show_grup';
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            } else {
+                $data['msg'][] = "Eroare la stergerea din grupa";
+                $data['redirect'][] = 'administrare/show_grup';
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            }
+            break;
+                
+        case 'delete_all':
+
+            $sterse = 0;
+            $nesterse = 0;
+            $grupa = new Grupa(getDbh());
+
+            foreach ($_POST as $key) {
+                if ($key == 'delete_all' || $key == 'Sterge tot') {
+                    continue;
+                } else {
+                    $grupaID = explode("_", $key);
+                    $deleteMember = $grupa->deleteGrupaMember($grupaID[1]);
+                    if ($deleteMember) {
+                        $sterse++;
+                    } else {
+                        $nesterse++;
+                    }
+                }
+            }
+
+            if ($sterse > 0) {
+                $data['msg'][] = $sterse . " studenti au fost stersi cu success";
+                $data['redirect'][] = 'administrare/show_grup';
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            } else {
+                $data['msg'][] = $nesterse . " studenti nu au fost stersi";
+                $data['redirect'][] = 'administrare/show_grup';
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
+            }
+            break;
     }
 }
