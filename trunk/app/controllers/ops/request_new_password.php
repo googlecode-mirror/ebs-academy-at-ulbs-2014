167b0f1baef98412af5cee91d350ba4f6abb6ba7
icontiu@gmail.com <icontiu@gmail.com@d08c0a24-99e4-fdea-bc22-444aa8f1480b>
2015-03-21 20:04:42 +0000

    $refs 34, bug 1,2,3

--- trunk/app/controllers/ops/request_new_password.php
+++ trunk/app/controllers/ops/request_new_password.php
@@ -7,20 +7,23 @@ function _request_new_password() {
         $ID = $user->checkEmail($_POST['email']);
         if (isset($ID['ID'])) {
             $setToken = $user->setRecover($ID['ID'], $_POST['email']);
-            if ($setToken == true) {
+
+            if ($setToken != false) {
                 $body = 'Pentru a schimba parola apasa   <a href="' . WEB_DOMAIN . WEB_FOLDER . 'ops/recover_password/' . $setToken . '"> AICI </a>';
                 if (sendEmail('Schimbare parola', $body, 'ulbsPlatform@ebs.ro', $_POST['email'])) {
                     $data['msg'][] = "Emailul cu linkul de resetare a parolei a fost trimis";
                     View::do_dump(VIEW_PATH . 'layout.php', $data);
                 } else {
-                    redirect('eroare');
+                    $data['msg'][] = "Emailul nu a fost trimis";
+                    View::do_dump(VIEW_PATH . 'layout.php', $data);
                 }
             } else {
-                redirect('eroare');
+                $data['msg'][] = "Tokenul este gresit sau au trecut mai mult de 2 zile de la cererea de recuperare parola";
+                View::do_dump(VIEW_PATH . 'layout.php', $data);
             }
-            redirect('main/index');
         } else {
-            redirect('eroare');
+            $data['msg'][] = "Acest user nu exista";
+            View::do_dump(VIEW_PATH . 'layout.php', $data);
         }
     } else {
         redirect('main/index');
